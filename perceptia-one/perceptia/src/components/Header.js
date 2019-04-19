76dabb32b74cd0a6f6b5920d105da26a40685c1e
Nuo Chen <nuo@D-10-19-207-153.dhcp4.washington.edu>
2019-04-19 15:17:19 -0700

    Continue working on components

--- perceptia-one/perceptia/src/components/Header.js
+++ perceptia-one/perceptia/src/components/Header.js
@@ -4,10 +4,39 @@ import NavBar from './NavBar'
 class Header extends React.Component {
     render() {
        return (
-          <div>
-             <h1>Header</h1>
-             <NavBar />
-          </div>
+          <h1>Perceptia</h1>
+         //  <div>
+         //     <h1>Header</h1>
+         //     <NavBar />
+         //  </div>
+
+         // <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+         // <a class="navbar-brand" href="#">
+
+         //    <img class="logo" src="img/branding/t-logo.png" alt="Thalesian Branding">
+ 
+         // </a>
+         // <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+         //     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         //     <span class="navbar-toggler-icon"></span>
+         // </button>
+ 
+         // <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         //     <ul class="navbar-nav ml-auto">
+         //         <li class="nav-item active">
+         //             <a class="nav-link" href="#team">Team<span class="sr-only">(current)</span></a>
+         //         </li>
+         //         <li class="nav-item active">
+         //             <a class="nav-link" href="#perceptia">Perceptia</a>
+         //         </li>
+         //         <li class="nav-item active">
+         //             <a class="nav-link" href="#code">Code</a>
+         //         </li>
+         //     </ul>
+         // </div>
+ 
+         // </nav>
+ 
        );
     }
  }
