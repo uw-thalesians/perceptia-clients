f693c79a5410a5e75348ad1ce1337d4694d13fb3
Nuo Chen <nuo@D-10-19-234-191.dhcp4.washington.edu>
2019-05-10 16:25:32 -0700

    End of ocelot

--- perceptia-one/perceptia/src/App.js
+++ perceptia-one/perceptia/src/App.js
@@ -24,7 +24,6 @@ import Question from './components/Question'
 
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 
-
 class App extends Component {
   render() {
     return (
@@ -43,6 +42,7 @@ class App extends Component {
           {/* <QuestionView/> */}
           <Route exact path='/' component={QuizGallery}/>
           <Route path='/quiz' component={QuestionView}/>
+          {/* <Route path='/summary' component={TopicDetail}/> */}
         </Switch>
         </Router>
 
