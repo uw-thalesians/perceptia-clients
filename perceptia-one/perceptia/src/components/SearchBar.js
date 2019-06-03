b6b58bdddcc9e57281ba6a02de639a4c2a39d385
Christopher Kites <chriskites@gmail.com>
2019-06-03 14:40:33 -0700

    Refactor search to material-ui

--- perceptia-one/perceptia/src/components/SearchBar.js
+++ perceptia-one/perceptia/src/components/SearchBar.js
@@ -1,5 +1,27 @@
-
 import React from 'react';
+import { InputBase, IconButton, Paper, withStyles } from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons/';
+
+const styles = {
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '8px'
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
+  },
+};
 
 class SearchBar extends React.Component {
 
@@ -14,56 +36,53 @@ class SearchBar extends React.Component {
     
     handleSubmit(evt) {
         evt.preventDefault();
-        this.props.onSearchQuiz(this.state.userInput);
-
+        if (this.state.userInput !== '') {
+          this.props.onSearchQuiz(this.state.userInput);
+        }
     }
 
     render() {
+      const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          {
+            this.state.errorMessage ?
+              <div className={"Center-Align"}>
+                {this.state.errorMessage}
+              </div> :
+              undefined
+          }
+          <InputBase
+            className={classes.input}
+            placeholder="Search Topics from Wiki..."
+            inputProps={{ 'aria-label': 'Search Topics from Wikipedia' }}
+            defaultValue={this.state.userInput}
+            onSubmit={evt => this.handleSubmit(evt)}
+            onKeyUp={evt => {
+              if(this.state.timeoutHandle) {
+                clearTimeout(this.state.timeoutHandle);
+              }
+              if (evt.key === 'Enter') {
+                this.handleSubmit(evt);
+                return;
+              }
 
-        return (
-            <form className="input-group md-form form-sm form-2 pl-0" onSubmit={evt => this.handleSubmit(evt)}>
-                {
-                    this.state.errorMessage ?
-                        <div className={"Center-Align"}>
-                            {this.state.errorMessage}
-                        </div> :
-                        undefined
-                }
-              
-                <input 
-                    className="form-control form-control-sm mr-3 w-75"
-                    type="text" 
-                    placeholder="Search..." 
-                    
-                    defaultValue={this.state.userInput}
-                    onKeyUp={evt => {
-                        if(this.state.timeoutHandle) {
-                            clearTimeout(this.state.timeoutHandle);
-                        }
-                        
-                        var value = evt.target.value;
-                        var handle = setTimeout((value)=>{this.props.onFilterQuiz(value);}, 200, value);
+              let value = evt.target.value;
+              let handle = setTimeout((value)=>{this.props.onFilterQuiz(value);}, 200, value);
 
-                        this.setState({
-                            userInput: evt.target.value,
-                            timeoutHandle: handle
-                    }); 
-                        //this.props.onFilterQuiz(evt.target.value);
-                    }} />
+              this.setState({
+                userInput: evt.target.value,
+                timeoutHandle: handle
+              });
+            }}
+          />
+          <IconButton className={classes.iconButton} aria-label="Search" onClick={evt => this.handleSubmit(evt)}>
+            <SearchIcon />
+          </IconButton>
 
-                {/* <div class="input-group-append">
-                    <span class="input-group-text lighten-2">
-                        Go!
-                    </span>
-                </div> */}
-            </form>
-
-        );
-
+        </Paper>
+      );
     }
-
-
-
 }
 
-export default SearchBar;
+export default withStyles(styles)(SearchBar);
