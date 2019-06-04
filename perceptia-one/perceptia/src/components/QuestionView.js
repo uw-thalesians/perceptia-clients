48ad3d0a3e5932b1107a01b3716a3a248230a102
Christopher Kites <chriskites@gmail.com>
2019-06-03 19:50:57 -0700

    Prep code for release
    Remove logging
    Remove redux store init
    Remove unnecessary references

--- perceptia-one/perceptia/src/components/QuestionView.js
+++ perceptia-one/perceptia/src/components/QuestionView.js
@@ -1,225 +1,208 @@
 import React from 'react';
-import {
-    LinearProgress, CircularProgress
-} from '@material-ui/core';
+import { LinearProgress, CircularProgress } from '@material-ui/core';
 
 import {Question, Results, Study} from './';
 import constants from './constants';
-
 import './quiz.css';
 
 
 class QuestionView extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedQuiz: null,
+      questions: [],
+      question: null,
+      questionId: null,
+      question_type: null,
+      answerOptions: [],
+      answers: {},
+      counter: 0,
+      paragraphs: null,
+      paragraph_index: 0,
+      paragraph: null,
+      completed: false,
+      grades: [],
+      mode: "quiz",
+      busy: false,
+    };
+  }
 
-        console.log(props);
-        this.state = {
-            selectedQuiz: null,
-            questions: [],
-            question: null,
-            questionId: null,
-            question_type: null,
-            answerOptions: [],
-            answers: {},
-            counter: 0,
-            paragraphs: null,
-            paragraph_index: 0,
-            paragraph: null,
-            completed: false,
-            grades: [],
-            mode: "quiz",
-            busy: false,
-        };
+  componentDidMount() {
+    const { selectedQuiz } = this.props.location.state;
+
+    let mode = this.props.location.state.mode;
+    let mode_url = "questions";
+
+    if(mode==="quiz") {
+      mode_url = "questions";
+    } else {
+      mode_url = "study";
     }
 
-    componentDidMount() {
+    fetch(`${constants.api.url}/api/v1/anyquiz/${mode_url}/${selectedQuiz}`)
+      .then(response => response.json())
+      .then(response => {
+        let paragraphs = null;
+        let paragraph = null;
 
-        console.log(this.props, this.state);
-
-        const { selectedQuiz } = this.props.location.state;
-        
-        var mode = this.props.location.state.mode;
-        var mode_url = "questions";
-
-        if(mode==="quiz") {
-            mode_url = "questions";
-        } else {
-            mode_url = "study";
+        if(Object.keys(response).includes("paragraphs")){
+          paragraphs = response.paragraphs;
+          paragraph = paragraphs[0];
         }
 
-        fetch(`${constants.api.url}/api/v1/anyquiz/${mode_url}/${selectedQuiz}`)
-            .then(response => response.json())
-            .then(response => {
-                
-                console.log("processing api response", response);
+        let questions = response.questions;
+        let p_id = response.questions[this.state.counter].p_id;
 
-                var paragraphs = null;
-                var paragraph = null;
-
-                if(Object.keys(response).includes("paragraphs")){
-                    paragraphs = response.paragraphs;
-                    paragraph = paragraphs[0];
-                }
-
-                var questions = response.questions;
-                var p_id = response.questions[this.state.counter].p_id;
-
-                this.setState({
-                    paragraphs: paragraphs,
-                    paragraph: paragraph,
-                    questions: questions,
-                    mode: mode,
-                    paragraph_id: p_id,
-                });
-
-                return response.questions;
-
-            })
-            .then(list => {
-                return list[this.state.counter];
-            })
-            .then(data => {
-                    this.setState({
-                    question: data.question,
-                    answerOptions: data.answer,
-                    question_type: +data.q_type,
-                    questionId: data.id,
-                    paragraph_id: this.state.questions[this.state.counter].p_id,
-                });
-            });
-
-    }
-
-    setNextQuestion() {
-        const counter = this.state.counter + 1;
-        
         this.setState({
-            counter: counter,
-            question: this.state.questions[counter].question,
-            answerOptions: this.state.questions[counter].answer,
-            questionId: this.state.questions[counter].id,
-            question_type: +this.state.questions[counter].q_type,
-            paragraph_id: this.state.questions[counter].p_id,
-            busy: false,
+          paragraphs: paragraphs,
+          paragraph: paragraph,
+          questions: questions,
+          mode: mode,
+          paragraph_id: p_id,
         });
 
-    };
+        return response.questions;
 
-    handleAnswerSelected(answer) {
-
-        if(answer==null){
-            this.setState({grades: this.state.grades.concat([false])});
-            this.setNextQuestion();
-            return;
-        }
-
-        var body = JSON.stringify({
-            "questionID":`${this.state.questionId}`,
-            "selectedAnswer": `${answer}`});
-
-        fetch(`${constants.api.url}/api/v1/anyquiz/questions/grade`, {
-            method: 'post',
-            body: body,
-        })
-        .then( response => response.json())
-        .then(response =>{
-
-            this.setState({grades: this.state.grades.concat([response["result"]])});
-
-        }).then(() => {
-            if (this.state.counter < this.state.questions.length - 1) {
-                this.setNextQuestion();
-            } else {
-                this.setState({
-                    completed:true,
-                    counter: this.state.counter+1
-                });
-            }
-            
+      })
+      .then(list => {
+        return list[this.state.counter];
+      })
+      .then(data => {
+        this.setState({
+          question: data.question,
+          answerOptions: data.answer,
+          question_type: +data.q_type,
+          questionId: data.id,
+          paragraph_id: this.state.questions[this.state.counter].p_id,
         });
-        
+      });
+
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+
+    this.setState({
+      counter: counter,
+      question: this.state.questions[counter].question,
+      answerOptions: this.state.questions[counter].answer,
+      questionId: this.state.questions[counter].id,
+      question_type: +this.state.questions[counter].q_type,
+      paragraph_id: this.state.questions[counter].p_id,
+      busy: false,
+    });
+
+  };
+
+  handleAnswerSelected(answer) {
+
+    if(answer==null){
+      this.setState({grades: this.state.grades.concat([false])});
+      this.setNextQuestion();
+      return;
     }
 
-    handlePairedQuestions(event) {
-        
-        if (this.state.paragraph_index < this.state.paragraphs.length - 1) {
-            this.setState({
-                paragraph_index: this.state.paragraph_index+1,
-            });
-            
-        } else {
-            
-            this.setState({
-                completed:true
-            });
-        }
-    };
+    var body = JSON.stringify({
+      "questionID":`${this.state.questionId}`,
+      "selectedAnswer": `${answer}`});
 
-    renderLogic() {
+    fetch(`${constants.api.url}/api/v1/anyquiz/questions/grade`, {
+      method: 'post',
+      body: body,
+    })
+      .then( response => response.json())
+      .then(response =>{
 
-        if (this.state.completed) {
+        this.setState({grades: this.state.grades.concat([response["result"]])});
 
-            return (
-                <Results questions={this.state.questions} grades={this.state.grades}/>
-            );
+      }).then(() => {
+      if (this.state.counter < this.state.questions.length - 1) {
+        this.setNextQuestion();
+      } else {
+        this.setState({
+          completed:true,
+          counter: this.state.counter+1
+        });
+      }
 
-        } else {
+    });
 
-            if(this.state.mode==="study" && this.state.paragraph_index <= this.state.counter){
-                
-                var psummary = this.state.paragraphs.filter((p)=> p.id === this.state.paragraph_id);
+  }
 
-                return <Study 
-                    quiz={this.props.location.state.selectedQuiz}
-                    summary={psummary}
-                    onNext={() => this.handlePairedQuestions()} />;
-            }
+  handlePairedQuestions() {
 
-            console.log(this.state);
+    if (this.state.paragraph_index < this.state.paragraphs.length - 1) {
+      this.setState({
+        paragraph_index: this.state.paragraph_index+1,
+      });
 
-            //uses key to tell react the question has changed and needs new object,
-            //look into memoization helper
-            return (
-                <Question key={"question_"+ this.state.questionId}
-                question={this.state.question}
-                question_type={this.state.question_type}
-                answerOptions={this.state.answerOptions}
-                questionId={this.state.questionId}
-                onAnswerSelected={(option)=>this.handleAnswerSelected(option)} 
-                busy={this.state.busy}/>
+    } else {
 
-            );
+      this.setState({
+        completed:true
+      });
+    }
+  };
 
-        }
-    };
+  renderLogic() {
 
-    render() {
-//<!-- className="App-header">-->
-        if (this.state.question != null) {
-            return(
-                <div className={"Center-Align"}>
-                    <div>
-                        <h2>{this.props.location.state.selectedQuiz} {this.state.mode==="quiz"?"quiz":"study"}</h2>
+    if (this.state.completed) {
 
-                        <div className="progress">
-                            <LinearProgress variant="determinate" value={this.state.counter*100/this.state.questions.length}/>
-                        </div>
-                    </div>
-                    {this.renderLogic()}
-                </div>
-            );
-        }
+      return (
+        <Results questions={this.state.questions} grades={this.state.grades}/>
+      );
 
-        return (
-            <div className={"Center-Align"}>
-                <CircularProgress/>
+    } else {
+
+      if(this.state.mode==="study" && this.state.paragraph_index <= this.state.counter){
+
+        let psummary = this.state.paragraphs.filter((p)=> p.id === this.state.paragraph_id);
+
+        return <Study
+          quiz={this.props.location.state.selectedQuiz}
+          summary={psummary}
+          onNext={() => this.handlePairedQuestions()} />;
+      }
+
+      //uses key to tell react the question has changed and needs new object,
+      //look into memoization helper
+      return (
+        <Question key={"question_"+ this.state.questionId}
+                  question={this.state.question}
+                  question_type={this.state.question_type}
+                  answerOptions={this.state.answerOptions}
+                  questionId={this.state.questionId}
+                  onAnswerSelected={(option)=>this.handleAnswerSelected(option)}
+                  busy={this.state.busy}/>
+
+      );
+    }
+  };
+
+  render() {
+    if (this.state.question != null) {
+      return(
+        <div className={"Center-Align"}>
+          <div>
+            <h2>{this.props.location.state.selectedQuiz} {this.state.mode==="quiz"?"quiz":"study"}</h2>
+
+            <div className="progress">
+              <LinearProgress variant="determinate" value={this.state.counter*100/this.state.questions.length}/>
             </div>
-        );
-
+          </div>
+          {this.renderLogic()}
+        </div>
+      );
     }
 
+    return (
+      <div className={"Center-Align"}>
+        <CircularProgress/>
+      </div>
+    );
+  }
 }
 
 export default QuestionView;
--- perceptia-one/perceptia/src/components/Study.js
+++ perceptia-one/perceptia/src/components/Study.js
@@ -1,33 +1,23 @@
 import React from 'react';
+import {Button, CircularProgress} from '@material-ui/core';
+import { NavigateNext } from "@material-ui/icons";
 
 import './quiz.css';
-import { NavigateNext } from "@material-ui/icons";
-import {Button, CircularProgress} from '@material-ui/core';
 
 
 class Study extends React.Component {
 
-    /*constructor(props) {
-        super(props);
+  render() {
+    if(this.props.quiz==null && this.props.summary==null) {
+      return (<CircularProgress/>);
+    }
 
-    }*/
-
-
-
-    render() {
-        //console.log("render this",this, this.props.quiz==null, this.props.summary==null);
-        if(this.props.quiz==null && this.props.summary==null) {
-            return (<CircularProgress/>);
-        }
-
-        return (
-            <div>
-                <h2 className="question">{(this.props.summary[0].text)}</h2>
-                <Button onClick={this.props.onNext}>Next <NavigateNext/></Button>
-            </div>
-
-        );
-
+    return (
+      <div>
+        <h2 className="question">{(this.props.summary[0].text)}</h2>
+        <Button onClick={this.props.onNext}>Next <NavigateNext/></Button>
+      </div>
+    );
   }
 }
 
